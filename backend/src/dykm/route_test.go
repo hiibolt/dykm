54920e1c642e542463ce6c7b47ff173811063566
Codins <shaivilisthebest@gmail.com>
2024-10-02 17:51:48 -0500

    feat: Created test for routing
    Works towards #4

---
+++ backend/src/dykm/route_test.go
@@ -0,0 +1,30 @@
+package main
+
+// import (
+// 	"log"
+// 	"testing"
+// )
+
+// func TestPrettyStrings(t *testing.T) {
+// 	tally := Tally{}
+
+// 	expected_output := `
+//   Usernames:  0
+//   Emails:     0
+//   Phones:     0
+//   Hashes:     0
+//   Salts:      0
+//   Ips:        0
+//   Names:      0
+//   Passwords:  0
+//   Addresses:  0
+//   Companies:  0
+//   Other:      0`
+
+// 	log.Println(expected_output)
+// 	log.Println(tally.String())
+// 	if expected_output != tally.String() {
+// 		t.Fatal("Output does not match expected output")
+// 	}
+
+// }
