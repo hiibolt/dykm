5649cca3371ec9f97985b80d84b296844b7d70eb
hiibolt <me@hiibolt.com>
2024-10-02 13:09:25 -0500

    feat: Added intrinsic add function to `Tally` type
    This targets goal 3 for #9.

--- backend/src/dykm/lib_api_response.go
+++ backend/src/dykm/lib_api_response.go
@@ -15,6 +15,23 @@ type Tally struct {
 	other     int32;
 };
 
+// Adds two Tallys together.
+func (tally1 Tally) add(tally2 Tally) Tally {
+	return Tally{
+		usernames: tally1.usernames + tally2.usernames,
+		emails:    tally1.emails + tally2.emails,
+		phones:    tally1.phones + tally2.phones,
+		hashes:    tally1.hashes + tally2.hashes,
+		salts:     tally1.salts + tally2.salts,
+		ips:       tally1.ips + tally2.ips,
+		names:     tally1.names + tally2.names,
+		passwords: tally1.passwords + tally2.passwords,
+		addresses: tally1.addresses + tally2.addresses,
+		companies: tally1.companies + tally2.companies,
+		other:     tally1.other + tally2.other,
+	};
+}
+
 // Converts an APIResponse to a human-readable string
 func (response Tally) String() string {
 	return "todo!();";
