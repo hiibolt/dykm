e59fb61714be17ba5e0ba34eae2c5b417222171e
Codins <shaivilisthebest@gmail.com>
2024-10-02 17:51:31 -0500

    feat, fix: Changed how request routing works
    Works towards #4

--- backend/src/dykm/route_tally.go
+++ backend/src/dykm/route_tally.go
@@ -5,29 +5,106 @@ func TallyResults ( req APIRequest) Result[Tally] {
     
     switch (req.PIIType) {
       case "email":
-        result.Emails += 0; //SnusbaseQuery(req.PII); Call to SnusbaseQuery
+        tally_result := SnusbaseQuery(req.PIIType, req.PII);
+        
+        if tally_result.IsErr(){
+          return tally_result;
+        }
+        var tally Tally = tally_result.UnwrapOk();
+        result.add(tally);
 
       case "phone":
         result.Phones += 0; // BulkVSQuery(req.PII);
 
       case "username":
-        result.Usernames += 0;// SherlockQuery(req.PII);
-        result.Usernames += 0;// SnusbaseQuery(req.PII);
+        tally_result1 := SnusbaseQuery(req.PIIType, req.PII);
+        // tally_result2 := SherlockQuery(req.PII);
+
+        if tally_result1.IsErr(){
+          return tally_result1;
+        }
+
+        // if tally_result2.IsErr(){
+        //   return tally_result2;
+        // }
+
+        //Unwrap both of the tallies
+        var tally1 Tally = tally_result1.UnwrapOk();
+        // var tally2 Tally = tally_result2.UnwrapOk();
+
+        //Add it to the final result
+        result.add(tally1);
+        // result.add(tally2);
 
       case "name":
-        result.Names += 0; // SnusbaseQuery(req.PII);
+        tally_result1 := SnusbaseQuery(req.PIIType, req.PII);
+
+        if tally_result1.IsErr(){
+          return tally_result1;
+        }
+
+        var tally1 Tally = tally_result1.UnwrapOk();
+        result.add(tally1);
 
       case "ip":
-        result.Ips += 0; // SnusbaseQuery(req.PII);
-        result.Ips += 0; // SnusbaseGeo(req.PII);
+        tally_result1 := SnusbaseQuery(req.PIIType, req.PII);
+        tally_result2 := SnusbaseGeo(req.PII);
+
+        if(tally_result1.IsErr()){
+          return tally_result1;
+        }
+
+        if(tally_result2.IsErr()){
+          return tally_result1;
+        }
+        
+        //Unwrap both tallies
+        var tally1 Tally = tally_result1.UnwrapOk();
+        var tally2 Tally = tally_result2.UnwrapOk();
+        
+        //Add it to the final result
+        result.add(tally1);
+        result.add(tally2);
 
       case "hash":
-        result.Hashes += 0; // SnusbaseQuery(req.PII);
-        result.Hashes += 0; // SnusbaseHashing(req.PII);
+        tally_result1 := SnusbaseQuery(req.PIIType, req.PII);
+        tally_result2 := SnusbaseHashing(req.PIIType, req.PII);
+
+        if(tally_result1.IsErr()){
+          return tally_result1;
+        }
+
+        if(tally_result2.IsErr()){
+          return tally_result1;
+        }
+        
+        //Unwrap both tallies
+        var tally1 Tally = tally_result1.UnwrapOk();
+        var tally2 Tally = tally_result2.UnwrapOk();
+        
+        //Add it to the final result
+        result.add(tally1);
+        result.add(tally2);
 
       case "password":
-        result.Passwords += 0; // SnusbaseQuery(req.PII);
-        result.Passwords += 0; //SnusBaseHashing(req.PII);
+        tally_result1 := SnusbaseQuery(req.PIIType, req.PII);
+        tally_result2 := SnusbaseHashing(req.PIIType, req.PII);
+
+        if(tally_result1.IsErr()){
+          return tally_result1;
+        }
+
+        if(tally_result2.IsErr()){
+          return tally_result1;
+        }
+        
+        //Unwrap both tallies
+        var tally1 Tally = tally_result1.UnwrapOk();
+        var tally2 Tally = tally_result2.UnwrapOk();
+        
+        //Add it to the final result
+        result.add(tally1);
+        result.add(tally2);
 
       default:
         break;
