b83ece98e58cced759b66b93dddd16bfc831732f
hiibolt <me@hiibolt.com>
2024-10-02 21:55:17 -0500

    feat: Complete HTML templating for the frontend

--- backend/src/dykm/main.go
+++ backend/src/dykm/main.go
@@ -1,21 +1,55 @@
 package main
 
 import (
+	"fmt"
 	"io"
 	"log"
 	"net/http"
+	"strings"
 )
 
-func hello_handler(w http.ResponseWriter, req *http.Request) {
+func public_handler(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, "../../../frontend/pages/index.html")
+}
+func page_handler(w http.ResponseWriter, r *http.Request) {
+	path_segments := strings.Split(r.URL.Path, "/")
+
+	// Home Directory
+	if len(path_segments) < 2 || path_segments[1] == "" {
+		http.ServeFile(w, r, "../../../frontend/pages/index.html")
+
+		return
+	}
+
+	switch path_segments[1] {
+	case "about", "app":
+		// Serve the respective page
+		http.ServeFile(w, r, "../../../frontend/pages/"+path_segments[1]+".html")
+	default:
+		// Serve the 404 page
+		http.ServeFile(w, r, "../../../frontend/pages/404.html")
+	}
+}
+func api_handler(w http.ResponseWriter, req *http.Request) {
 	io.WriteString(w, "Hello, world!\n")
 }
 
 func main() {
-	var port string = ":8000";
+	var port string = ":8000"
 
-	http.HandleFunc("/hello", hello_handler);
+	http.Handle(
+		"/public/",
+		http.StripPrefix("/public/", http.FileServer(http.Dir("../../../frontend/public"))),
+	)
+	http.HandleFunc(
+		"/",
+		page_handler,
+	)
+	http.HandleFunc(
+		"/api",
+		api_handler,
+	)
 
-    log.Println("Listing for requests at http://localhost:8000/hello");
-	
-	log.Fatal(http.ListenAndServe(port, nil));
+	log.Println(fmt.Sprintf("Listing for requests at http://localhost%s/!", port))
+	log.Fatal(http.ListenAndServe(port, nil))
 }
