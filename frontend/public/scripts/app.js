cac4f04d2be1b1d189441f59a4c87f4f3e3379fd
alex-reynoso <reynosoa101@yahoo.com>
2024-10-03 23:02:56 -0500

    feat. Implemeted API call to recieve PII data

--- frontend/public/scripts/app.js
+++ frontend/public/scripts/app.js
@@ -6,17 +6,20 @@ document.getElementById('TallyForm').addEventListener('submit', function(event) 
 
     // Collect the form data from the form elements
     const form = event.target;  // This is the form that triggered the event
-    const formData = new FormData(form);  // Automatically picks up all form inputs
+    const formData = new FormData(form);
+    const urlEncodedData = new URLSearchParams(); // Automatically picks up all form inputs
 
     // Logging form data for debugging
+
     for (const entry of formData.entries()) {
+        urlEncodedData.append(entry[0], entry[1]);
         console.log(entry[0], entry[1]);
     }
     
     // Send the form data using the Fetch API
     fetch('/api/tally', {  // The backend URL
         method: 'POST',
-        body: formData,
+        body: urlEncodedData,
         headers: {
             "Content-Type": "application/x-www-form-urlencoded"
         }
@@ -24,9 +27,20 @@ document.getElementById('TallyForm').addEventListener('submit', function(event) 
     })
     .then(response => response.json()) // Parse the response as JSON
     .then(data => {
+        console.log(data)
+
         // Display the response or handle it as needed
         document.getElementById('response').innerText = "Form submitted successfully!";
+        document.getElementById('usernames').innerText = "Usernames: " + data.usernames; 
+        document.getElementById('emails').innerText = "Emails: " + data.emails;
+        document.getElementById('phones').innerText = "Phones: " + data.phones;
+        document.getElementById('hashes').innerText = "Hashes: " + data.hashes;
+        document.getElementById('ips').innerText = "Ips: " + data.ips;
+        document.getElementById('names').innerText = "Names: " + data.names;
+        document.getElementById('passwords').innerText = "Passwords: " + data.passwords;
+
     })
+
     .catch(error => {
         // Handle any errors
         document.getElementById('response').innerText = "Error submitting the form.";
