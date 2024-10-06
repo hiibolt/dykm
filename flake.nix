6b8b7dd21336077feb3b245e2166713aa1d1dec1
hiibolt <me@hiibolt.com>
2024-10-05 21:46:02 -0500

    chore: Prettied up the Nix environment

--- flake.nix
+++ flake.nix
@@ -21,11 +21,17 @@
         # Custom Colors & Characters
         export RESET='\033[00m'
         export PINK='\033[01;35m'
+        export CYAN='\033[01;36m'
         export END='❯'
         export RED='\033[0;31m'
 
-        # Banner
-        printf "$RESET""[ Nix Flake ]\n"
+        # Banner Function
+        burger() {
+          printf "$RESET""[ Nix Flake ]\n"
+          printf "$PINK""run$RESET    | ""$PINK""r""$RESET""\n- Starts the Go Webserver\n"
+          printf "$PINK""test$RESET   | ""$PINK""t""$RESET""\n- Runs the tests in all files ending in \`test.go\`\n"
+          printf "$PINK""build$RESET  | ""$PINK""b""$RESET""\n- Builds the Go Webserver, producing \`./dykm\`\n\n"
+        }
 
         # Verify WD is `*/dykm`
         verify_pw() { [[ "$PWD" == */dykm ]] && { return 0; } || { echo "You aren't in the repository root \`./dykm\`!"; return 1; }; }
@@ -59,21 +65,19 @@
             verify_pw && start_application_in_build
         }
         alias r="run"
-        printf "$PINK""run$RESET   | ""$PINK""r""$RESET""\n- Starts the Go Webserver\n"
 
         # Test Command
-        alias test="verify_pw && ./build.sh && cd ./build && go test -count=1 .; cd - > /dev/null 2>&1"
+        alias test="verify_pw && ./build.sh && cd ./build && go test -count=1 . && cd - > /dev/null 2>&1"
         alias t="test"
-        printf "$PINK""test$RESET  | ""$PINK""t""$RESET""\n- Runs the tests in all files ending in \`_test.go\`\n"
-
+        
         # Build Command
         alias build="verify_pw && rm -f ./dykm > /dev/null 2>&1 && ./build.sh && cd ./build && go build . && mv ./dykm ../dykm && cd - > /dev/null 2>&1 && ls dykm"
         alias b="build"
-        printf "$PINK""build$RESET | ""$PINK""b""$RESET""\n- Builds the Go Webserver, producing \`./dykm\`\n\n"
 
+        burger
 
         # Set custom PS1
-        export PS1="$RESET(\h) $PINK\w$RESET\$(if [ \$? -eq 0 ]; then echo -n '$RESET'; else echo -n '$RED'; fi) $END$RESET "
+        export PS1="$RESET$CYAN(nix) $PINK\w$RESET\$(if [ \$? -eq 0 ]; then echo -n '$RESET'; else echo -n '$RED'; fi) $END$RESET "
         '';
       RUST_SRC_PATH = pkgs.rustPlatform.rustLibSrc;
     };
